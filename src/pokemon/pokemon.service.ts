b8bcd9d3633ab08129dcf3b05db0ecebdcaba54b
Leonardo Omaña <leotangram@gmail.com>
2023-09-27 20:19:45 -0500

    create seed module

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -74,9 +74,6 @@ export class PokemonService {
   }
 
   async remove(id: string) {
-    // const pokemon = await this.findOne(id);
-    // await pokemon.deleteOne();
-    // const result = await this.pokemonModel.findByIdAndDelete(id);
     const { deletedCount } = await this.pokemonModel.deleteOne({ _id: id });
 
     if (deletedCount === 0) {
