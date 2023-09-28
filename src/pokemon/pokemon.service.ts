3b0525fa03bb5fe54ee131cffcc238c26c910765
Leonardo Omaña <leotangram@gmail.com>
2023-09-27 19:57:21 -0500

    custom pipes

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -74,8 +74,11 @@ export class PokemonService {
   }
 
   async remove(id: string) {
-    const pokemon = await this.findOne(id);
-    await pokemon.deleteOne();
+    // const pokemon = await this.findOne(id);
+    // await pokemon.deleteOne();
+    const result = this.pokemonModel.findByIdAndDelete(id);
+
+    return result;
   }
 
   private handleExceptions(error: any) {
