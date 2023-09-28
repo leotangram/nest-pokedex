8c3e3d762d5a03c6d3b998d2ddd2e25d02774a46
Leonardo Omaña <leotangram@gmail.com>
2023-09-27 20:02:32 -0500

    validate and delete in one consult

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -76,9 +76,14 @@ export class PokemonService {
   async remove(id: string) {
     // const pokemon = await this.findOne(id);
     // await pokemon.deleteOne();
-    const result = this.pokemonModel.findByIdAndDelete(id);
+    // const result = await this.pokemonModel.findByIdAndDelete(id);
+    const { deletedCount } = await this.pokemonModel.deleteOne({ _id: id });
 
-    return result;
+    if (deletedCount === 0) {
+      throw new BadRequestException(`Pokemon with id '${id}' not found`);
+    }
+
+    return;
   }
 
   private handleExceptions(error: any) {
