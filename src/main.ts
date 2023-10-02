d16be12ea3e8c931ca292943fde28939bb1b20b9
Leonardo Omaña <leotangram@gmail.com>
2023-10-02 15:09:40 -0500

    deploy

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT || 3002);
+  await app.listen(process.env.PORT);
   console.log(`App running on port ${process.env.PORT}`);
 }
 bootstrap();
